3292047c7ae0a0ca7fef36021a7079a700ff5c02
MakavchikDenis <makavchik1.19@gmail.com>
2021-01-28 20:40:15 +0300

    1

---
+++ src/main/java/MyFramwork.java
@@ -0,0 +1,10 @@
+
+
+public class MyFramwork {
+    public static void main(String args[]){
+        System.out.println("Hello world!! This is my Framwork!!");
+        System.out.println("!!!!!!!");
+
+
+    }
+}
